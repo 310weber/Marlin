733cc55f5f5a2b4691d5f6df1472930cba58cccd
Arnie Weber <310weber@gmail.com>
2021-05-24 09:15:03 -0400

    Reset MBL margin to 10 mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1062,7 +1062,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 5    //[AW] was 10
+#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
@@ -1509,7 +1509,7 @@
   //=================================== Mesh ==================================
   //===========================================================================
 
-  #define MESH_INSET 5          // Set Mesh bounds as an inset region of the bed    //[AW] was 10
+  #define MESH_INSET 10          // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 7    // Don't use more than 7 points per axis, implementation limited.  //[AW] was 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
